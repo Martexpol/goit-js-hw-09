8f8a581dd64118538cd54240e20acc52ef927dc4
Martexpol <150294627+Martexpol@users.noreply.github.com>
2024-04-20 11:02:29 +0000

    Deploying to gh-pages from @ Martexpol/goit-js-hw-09@12e63846943c9d22d172c0c4e4ac71f4cbd571df 🚀

---
+++ 01-color-switcher.34dc5d8c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let r=null;function d(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(()=>{r=setInterval(d,1e3),t.setAttribute("disabled",""),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled","")}));
+//# sourceMappingURL=01-color-switcher.34dc5d8c.js.map
